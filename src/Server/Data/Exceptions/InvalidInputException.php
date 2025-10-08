11a4ab7224add8dee490fb5a09a18491a2f6d16b
Leo Studer <leodaniel@nitrosoft.ch>
2025-10-08 17:54:22 +0200

    Added easier way to create an invalid input exception

--- src/Server/Data/Exceptions/InvalidInputException.php
+++ src/Server/Data/Exceptions/InvalidInputException.php
@@ -3,11 +3,23 @@
 namespace Le0daniel\PhpTsBindings\Server\Data\Exceptions;
 
 use Le0daniel\PhpTsBindings\Executor\Data\Failure;
+use Le0daniel\PhpTsBindings\Executor\Data\Issues;
 
 final class InvalidInputException extends \Exception
 {
     public function __construct(public readonly Failure $failure)
     {
         parent::__construct("Input validation failed", 422, $this->failure);
+    }
+
+    /**
+     * @param array<string, string|string[]> $issuesMap
+     * @return self
+     */
+    public static function createFromMessages(array $issuesMap): self
+    {
+        return new self(
+            new Failure(Issues::fromMessages($issuesMap)),
+        );
     }
 }
