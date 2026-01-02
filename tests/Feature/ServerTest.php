e01ee53486e26cacb5f7bdaccdc032d8912dfab7
Leo Studer <leodaniel@nitrosoft.ch>
2026-01-02 09:07:16 +0100

    Add test for TypeScript middleware emission and refine exception handling test

--- tests/Feature/ServerTest.php
+++ tests/Feature/ServerTest.php
@@ -2,6 +2,7 @@
 
 use Le0daniel\PhpTsBindings\Server\Client\NullClient;
 use Le0daniel\PhpTsBindings\Server\Data\ErrorType;
+use Le0daniel\PhpTsBindings\Server\Data\OperationType;
 use Le0daniel\PhpTsBindings\Server\Data\RpcError;
 use Le0daniel\PhpTsBindings\Server\Data\RpcSuccess;
 use Le0daniel\PhpTsBindings\Server\KeyGenerators\PlainlyExposedKeyGenerator;
@@ -9,7 +10,7 @@ use Le0daniel\PhpTsBindings\Server\Operations\EagerlyLoadedRegistry;
 use Le0daniel\PhpTsBindings\Server\Presenter\ClientAwareExceptionPresenter;
 use Le0daniel\PhpTsBindings\Server\Server;
 
-test("Exceptions are cought from middleware", function () {
+test("Exceptions are exposed through middleware", function () {
     $server = new Server(
         EagerlyLoadedRegistry::eagerlyDiscover(
             __DIR__ . '/Operations',
@@ -23,7 +24,8 @@ test("Exceptions are cought from middleware", function () {
     $result = $server->command('test.run', ['name' => 'Leo'], null, new NullClient());
 
     expect($result)->toBeInstanceOf(RpcSuccess::class)
-        ->and($result->data)->toEqual((object) ['message' => 'Hello Leo']);
+        ->and($result->data)
+        ->toEqual((object) ['message' => 'Hello Leo']);
 
     $error = $server->command('test.run', ['name' => 'invalid'], null, new NullClient());
 
@@ -32,4 +34,21 @@ test("Exceptions are cought from middleware", function () {
         ->and($error->details)->toEqual([
             'type' => 'invalid_name',
         ]);
+});
+
+test("Middleware emits typescript middleware", function () {
+    $server = new Server(
+        EagerlyLoadedRegistry::eagerlyDiscover(
+            __DIR__ . '/Operations',
+            keyGenerator: new PlainlyExposedKeyGenerator
+        ),
+        [
+            new ClientAwareExceptionPresenter(),
+        ],
+    );
+
+    $operation = $server->registry->get(OperationType::COMMAND, 'test.run');
+    $errorPresenter = new ClientAwareExceptionPresenter();
+    $definition = $errorPresenter->toTypeScriptDefinition($operation->definition);
+    expect($definition)->toEqual('{type: "invalid_name"}');
 });
